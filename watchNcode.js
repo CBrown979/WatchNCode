4a950fbc3d145ccba6a9591d90bd636b213c5041
Candice Bianca Brown <candicebrown97@gmail.com>
2017-11-15 11:48:28 +0000

    create a way to change existing todos

--- watchNcode.js
+++ watchNcode.js
@@ -7,5 +7,9 @@ var todos = ['item1', 'item2', 'item3'];
 
 //Way to add new todos -- use push command on the array
 todos.push('item4', 'item5');
-console.log("My Todos:", todos);
+//console.log("My Todos:", todos);
 //Output: My Todos: [ 'item1', 'item2', 'item3', 'item4', 'item5' ]
+
+//Way to change existing todos -- 1)isolate targeted todos via zero-index bracket, 2)add = sign and the new value
+todos[0] = "item 1 updated";
+console.log("My Todos:", todos);
