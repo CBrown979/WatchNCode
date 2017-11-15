e7963e84d8e36de5be74ff79818cf8bd2e95f956
Candice Bianca Brown <candicebrown97@gmail.com>
2017-11-15 11:36:12 +0000

    create a way to add new todos

--- watchNcode.js
+++ watchNcode.js
@@ -1,7 +1,11 @@
 //Place to store todos
-
 var todos = ['item1', 'item2', 'item3'];
 
 //Place to display todos
-console.log("My Todos:", todos);
+//console.log("My Todos:", todos);
 //Output: My Todos: [ 'item1', 'item2', 'item3' ]
+
+//Way to add new todos -- use push command on the array
+todos.push('item4', 'item5');
+console.log("My Todos:", todos);
+//Output: My Todos: [ 'item1', 'item2', 'item3', 'item4', 'item5' ]
